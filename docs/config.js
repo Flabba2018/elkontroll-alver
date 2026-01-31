ac21f22ed18dae715f9454736432a871ab696702
Flabba2018 <151993480+Flabba2018@users.noreply.github.com>
2026-01-31 18:17:14 +0100

    Update Supabase key in config.js
    Updated the Supabase key in the configuration file.

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://rkznfraztzrkzednukoc.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'; // Bruk din eksisterande anon key
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJrem5mcmF6dHpya3plZG51a29jIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njk4MDMxNTMsImV4cCI6MjA4NTM3OTE1M30._rhPweVYw-xvZ09GMT8nn2tsUBT84GA0CEn3zNN9gAw'; // Bruk din eksisterande anon key
 
 const APP_VERSION = '4.1.0-ALVER-PRO';
 
